0f0153a8bae5eff638f7a96cc2b23b2c26c21850
uhessbai <auriff.g@gmail.com>
2025-09-27 17:19:52 +0200

    opti loading points

--- dist/_worker.js/index.js
+++ dist/_worker.js/index.js
@@ -1,7 +1,7 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_C8e8paaU.mjs';
-import { manifest } from './manifest_dEb6P3nz.mjs';
+import { manifest } from './manifest_BWbXWZIU.mjs';
 
 const serverIslandMap = new Map();;
 
